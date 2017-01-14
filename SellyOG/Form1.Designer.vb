c66739d5545db43d939a6a3f93fa08e829921dc2
BuildTools <unconfigured@null.spigotmc.org>
2017-01-14 14:50:31 -0500

    Add project files.

---
+++ SellyOG/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Input = New System.Windows.Forms.ListBox()
+        Me.Output = New System.Windows.Forms.ListBox()
+        Me.Import = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Input
+        '
+        Me.Input.FormattingEnabled = True
+        Me.Input.Location = New System.Drawing.Point(46, 32)
+        Me.Input.Name = "Input"
+        Me.Input.Size = New System.Drawing.Size(146, 446)
+        Me.Input.TabIndex = 0
+        '
+        'Output
+        '
+        Me.Output.FormattingEnabled = True
+        Me.Output.Location = New System.Drawing.Point(439, 32)
+        Me.Output.Name = "Output"
+        Me.Output.Size = New System.Drawing.Size(146, 446)
+        Me.Output.TabIndex = 1
+        '
+        'Import
+        '
+        Me.Import.Location = New System.Drawing.Point(76, 484)
+        Me.Import.Name = "Import"
+        Me.Import.Size = New System.Drawing.Size(75, 23)
+        Me.Import.TabIndex = 2
+        Me.Import.Text = "Import"
+        Me.Import.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(478, 484)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Export"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(274, 85)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 68)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Start"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(274, 216)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 68)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Stop"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(677, 534)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Import)
+        Me.Controls.Add(Me.Output)
+        Me.Controls.Add(Me.Input)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "SellyOG Checker by Shadow"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Input As ListBox
+    Friend WithEvents Output As ListBox
+    Friend WithEvents Import As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ SellyOG/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Threading
+Imports System.Net
+Imports System.IO
+Public Class Form1
+
+    Dim startThread As Thread
+    Dim i As Integer
+    Dim SF As New SaveFileDialog
+    Private Sub Import_Click(sender As Object, e As EventArgs) Handles Import.Click
+        Using FD As New OpenFileDialog()
+            FD.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+            If FD.ShowDialog = System.Windows.Forms.DialogResult.OK Then
+                Input.Items.Clear()
+                Input.Items.AddRange(IO.File.ReadAllLines(FD.FileName))
+            End If
+        End Using
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        startThread = New Thread(AddressOf StartCheck)
+        startThread.Start()
+        Button3.Enabled = True
+        Button2.Enabled = False
+    End Sub
+
+    Private Sub StartCheck()
+
+        For i = 0 To Input.Items.Count - 1
+            Dim name As String = CStr(Input.Items(i))
+            Try
+                Dim Request As HttpWebRequest = DirectCast(WebRequest.Create("http://selly.gg/u/" + name), HttpWebRequest)
+                Dim Response As HttpWebResponse = Request.GetResponse()
+                Response.Close()
+            Catch ex As System.Net.WebException
+                If ex.Status = 7 Then
+                    Output.Items.Add(name)
+                    Continue For
+                End If
+            End Try
+        Next
+        Button2.Enabled = True
+        Button3.Enabled = False
+        MsgBox("Finished!")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckForIllegalCrossThreadCalls = False
+        Button2.Enabled = True
+        Button3.Enabled = False
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        i = Input.Items.Count
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SF.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+        If SF.ShowDialog() = DialogResult.OK Then
+            Using writer = New StreamWriter(SF.FileName)
+                For Each o As Object In Output.Items
+                    writer.WriteLine(o)
+                Next
+            End Using
+        End If
+
+    End Sub
+End Class
